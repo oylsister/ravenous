b515c0400b4a342d6c506c7d89029ff19aa03a56
James P. Naphat <kkuiba2009@gmail.com>
2024-08-13 15:58:55 +0700

    Unfinished Styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './Business.css';
+
+const pizza = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
 
 function App() {
   return (
@@ -25,15 +38,21 @@ function App() {
 function Business() {
   return (
     <div className='restaurant-container'>
-      <img className="restaurant-pic" src="" />
+      <div className='restaurant-pic-container'>
+        <img className="restaurant-pic" src={pizza.imageSrc} />
+      </div>
       <div className="info-container">
-        <h3 className='restaurant-name'>Ratchada Thai Food</h3>
+        <h3 className='restaurant-name'>{pizza.name}</h3>
         <div className="info-row">
-          <p className='info-location'></p>
+          <div className='location-container'>
+            <p className='info-location'>{pizza.address}</p>
+            <p className='info-location'>{pizza.city}</p>
+            <p className='info-location'>{pizza.state} {pizza.zipCode}</p>
+          </div>
           <div className="info-review">
-            <p className="food-type"></p>
-            <p className='food-rating'></p>
-            <p className='food-reviewcount'></p>
+            <p className="food-type">{pizza.category}</p>
+            <p className='food-rating'>{pizza.rating}</p>
+            <p className='food-reviewcount'>{pizza.reviewCount}</p>
           </div>
         </div>
       </div>
@@ -41,4 +60,4 @@ function Business() {
   );
 }
 
-export default { App, Business };
+export { App, Business };
